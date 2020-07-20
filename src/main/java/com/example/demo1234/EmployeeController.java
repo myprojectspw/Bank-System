98df928a1882f05dac65be62ede99196dbe750d9
p.wolski@polsource.com <pa-wo97@o2.pl>
2020-07-20 23:46:27 +0200

    Add all mappings

--- src/main/java/com/example/demo1234/EmployeeController.java
+++ src/main/java/com/example/demo1234/EmployeeController.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -21,10 +22,42 @@ public class EmployeeController {
     @Autowired
     EmployeeRepository employeeRepository;
 
-    @GetMapping("/list")
+    @GetMapping("/emp")
     public List<Employee> listEmployeesAll() {
         return employeeRepository.findAll();
     }
 
+    @GetMapping("/emp/{id}")
+    public Employee getOneEmployee(@PathVariable String id) {
+        int blogId = Integer.parseInt(id);
+        return employeeRepository.findOne(blogId);
+    }
+
+    @PostMapping("/emp")
+    public List<Employee> create(@RequestBody Map<String, String> body) {
+        String name = body.get("name");
+        String surname = body.get("surname");
+        String email = body.get("email");
+        int money = 0;
+        employeeRepository.save(new Employee(name, surname, email, money));
+        return employeeRepository.findAll();
+    }
+
+    @PutMapping("/emp/{id}")
+    public Employee update(@PathVariable String id, @RequestBody Map<String, String> body) {
+        int blogId = Integer.parseInt(id);
+        // getting blog
+        Employee emp = employeeRepository.findOne(blogId);
+        emp.setName(body.get("name"));
+        emp.setSurname(body.get("surname"));
+        return employeeRepository.save(emp);
+    }
+
+    @DeleteMapping("emp/{id}")
+    public boolean delete(@PathVariable String id) {
+        int empId = Integer.parseInt(id);
+        employeeRepository.delete(empId);
+        return true;
+    }
 
 }
