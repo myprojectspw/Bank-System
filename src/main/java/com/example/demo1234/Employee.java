295429ead864e97d559db35b019fd06ccdee5bc0
p.wolski@polsource.com <pa-wo97@o2.pl>
2020-08-04 18:26:05 +0200

    delete autogenerated

--- src/main/java/com/example/demo1234/Employee.java
+++ src/main/java/com/example/demo1234/Employee.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 public class Employee {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private int id;
 
